a4f70fd098f09c6a0cc6990aeb60b4ae020420f0
AIXAL <aixa.lievana@gmail.com>
2020-03-10 12:52:53 -0600

    :)

---
+++ public/main.js
@@ -0,0 +1,62 @@
+var socket = io.connect('http://localhost:8081', {
+    'forceNew': true
+});
+
+socket.on('messages', function (data) {
+    document.getElementById("imagen").setAttribute("src", "");
+    console.log(data);
+    document.getElementById("imagen").setAttribute("src", `img/${data.fileName}`);
+})
+
+function render(file) {
+    var html = `<div>
+                <img src="data:image/jpg;base64,${file}" /></div>`
+    document.getElementById('primera').innerHTML=html;
+}
+
+function addMessage(e) {
+    base64()
+    async function base64(){
+        var fileBase64 = await getBase64(document.getElementById('texto').files[0])
+        fileBase64 = fileBase64.split(",")[1];
+        console.log(fileBase64)
+        message(fileBase64)
+        render(fileBase64)
+        return fileBase64;
+    }
+
+    function message(text){
+        var extension = document.getElementById('texto').files[0].name.split('.').pop().toLowerCase();
+        var nombre = document.getElementById('texto').files[0].name;
+        var nombre2= nombre.split(".");
+         var message = {
+             img: text,
+             name: nombre2[0],
+         };
+         socket.emit('new-message', message);
+    }
+   document.getElementById("imagen").setAttribute("src", "");
+    return false;
+}
+
+async function base64(file){
+    var fileBase64 = await getBase64(file)
+    fileBase64 = fileBase64.split(",")[1];
+    console.log(fileBase64)
+    return fileBase64;
+}
+
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            // console.log(reader.result);
+            resolve(reader.result);
+        };
+        reader.onerror = function (error) {
+            // console.log('Error: ', error);
+            reject(null);
+        };
+    });
+}
---
+++ server/main.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var base64ToImage = require('base64-to-image');
+
+app.use(express.static('public'));
+
+app.get('/hello', function (req, res) {
+    res.status(200).send("Hello World!");
+});
+io.on('connection', function (socket) {
+    console.log('Alguien se ha conectado con Sockets');
+    // socket.emit('messages', messages);
+    socket.on('new-message', function (data) {
+        console.log(data)
+        var base64 ="data:image/jpg;base64,"+data.img
+        var path = "C:/Users/Aixa Lievana/Desktop/Proyecto2/public/img/";
+        console.log(__dirname);
+        var optional= {'fileName': data.name, 'type':'jpg'}
+
+       var image= base64ToImage(base64,path,optional)
+
+        io.sockets.emit('messages',image );
+    });
+});
+server.listen(8081, function () {
+    console.log("Servidor corriendo en http://localhost:8080");
+});
